eaa16485ec1310f0275f644ce325799261355129
ShirlyB-24-tech <160262117+ShirlyB-24-tech@users.noreply.github.com>
2024-04-30 18:11:25 +0200

    re installed boostrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> hello World</h1>
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Shirly B.
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
